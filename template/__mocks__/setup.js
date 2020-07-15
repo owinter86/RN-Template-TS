043173d444a7a85fe1ffe6040c33e2f217bf2909
Oliver Winter <owinter86@gmail.com>
2020-07-15 11:04:27 +0200

    Fix jest testing environment

--- template/__mocks__/setup.js
+++ template/__mocks__/setup.js
@@ -1,13 +1,93 @@
-jest.useFakeTimers();
+global.navigator = { geolocation: { getCurrentPosition: jest.fn() } };
+global.console.warn = jest.fn();
+
+jest.mock('react-native-gesture-handler', () => {
+  const { View, TouchableWithoutFeedback } = require('react-native');
+  return {
+    Swipeable: View,
+    DrawerLayout: View,
+    State: {},
+    ScrollView: View,
+    Slider: View,
+    Switch: View,
+    TextInput: View,
+    ToolbarAndroid: View,
+    ViewPagerAndroid: View,
+    DrawerLayoutAndroid: View,
+    WebView: View,
+    NativeViewGestureHandler: View,
+    TapGestureHandler: View,
+    FlingGestureHandler: View,
+    ForceTouchGestureHandler: View,
+    LongPressGestureHandler: View,
+    PanGestureHandler: View,
+    PinchGestureHandler: View,
+    RotationGestureHandler: View,
+    TouchableWithoutFeedback,
+    /* Buttons */
+    RawButton: View,
+    BaseButton: View,
+    RectButton: View,
+    BorderlessButton: View,
+    /* Other */
+    FlatList: View,
+    gestureHandlerRootHOC: jest.fn(),
+    Directions: {},
+  };
+});
+
+jest.mock('react-native-device-info', () => {
+  return {
+    getVersion: () => '',
+    getUniqueId: () => '',
+  };
+});
+
+jest.mock('@react-native-community/async-storage', () => {
+  const mockAsyncStorage = require('@react-native-community/async-storage/jest/async-storage-mock');
+  return mockAsyncStorage;
+});
 
 jest.mock('react-native-reanimated', () => {
   const animatedMock = require('react-native-reanimated/mock');
-  animatedMock.default.addWhitelistedNativeProps = jest.fn();
   return animatedMock;
+});
+
+jest.mock('react-native-config', () => ({
+  API_ENDPOINT: 'string',
+}));
+
+jest.mock('react-native-code-push', () => {
+  const cp = () => (app) => app;
+  Object.assign(cp, {
+    InstallMode: {},
+    CheckFrequency: {},
+    SyncStatus: {},
+    UpdateState: {},
+    DeploymentStatus: {},
+    DEFAULT_UPDATE_DIALOG: {},
+
+    allowRestart: jest.fn(),
+    checkForUpdate: jest.fn(() => Promise.resolve(null)),
+    disallowRestart: jest.fn(),
+    getCurrentPackage: jest.fn(() => Promise.resolve(null)),
+    getUpdateMetadata: jest.fn(() => Promise.resolve(null)),
+    notifyAppReady: jest.fn(() => Promise.resolve()),
+    restartApp: jest.fn(),
+    sync: jest.fn(() => Promise.resolve(1)),
+    clearUpdates: jest.fn(),
+  });
+  return cp;
+});
+
+jest.mock('@react-native-community/netinfo', () => {
+  const netInfoMock = require('@react-native-community/netinfo/jest/netinfo-mock.js');
+  return netInfoMock;
 });
 
 jest.mock('react-native-bootsplash', () => ({
   hide: jest.fn(),
+  show: jest.fn(),
 }));
 
 jest.mock('react-native-screens', () => ({
@@ -15,66 +95,20 @@ jest.mock('react-native-screens', () => ({
   screensEnabled: () => true,
 }));
 
-jest.mock('react-native-maps', () => 'AirGoogleMaps');
+jest.mock('bugsnag-react-native', () => ({
+  Configuration: jest.fn(),
+  Client: jest.fn(() => ({ leaveBreadcrumb: jest.fn() })),
+}));
 
-jest.mock('react-native-modal', () => 'Modal');
-
-jest.mock('@react-native-community/netinfo', () => {
-  const netInfoMock = require('@react-native-community/netinfo/jest/netinfo-mock.js');
-  return netInfoMock;
-});
-
-jest.mock('react-native-linear-gradient', () => 'BVLinearGradient');
-
-jest.mock('react-native-device-info', () => {
-  return {
-    getModel: jest.fn(),
-    getVersion: jest.fn(),
+jest.mock('reactotron-react-native', () => {
+  const reactotron = {
+    configure: jest.fn(() => reactotron),
+    useReactNative: jest.fn(() => reactotron),
+    use: jest.fn(() => reactotron),
+    connect: jest.fn(() => reactotron),
+    clear: jest.fn(() => reactotron),
+    createEnhancer: jest.fn(() => reactotron),
+    setAsyncStorageHandler: jest.fn(() => reactotron),
   };
+  return reactotron;
 });
-
-jest.mock('react-native-config', () => ({
-  API_ENDPOINT: 'https://api.tst.amstel-services.net',
-}));
-
-jest.mock('react-native-keychain', () => ({
-  SECURITY_LEVEL_ANY: 'MOCK_SECURITY_LEVEL_ANY',
-  SECURITY_LEVEL_SECURE_SOFTWARE: 'MOCK_SECURITY_LEVEL_SECURE_SOFTWARE',
-  SECURITY_LEVEL_SECURE_HARDWARE: 'MOCK_SECURITY_LEVEL_SECURE_HARDWARE',
-  setGenericPassword: jest.fn().mockResolvedValue(),
-  getGenericPassword: jest.fn().mockResolvedValue(),
-  resetGenericPassword: jest.fn().mockResolvedValue(),
-}));
-
-jest.mock('@react-native-community/async-storage', () => {
-  const mockAsyncStorage = require('@react-native-community/async-storage/jest/async-storage-mock');
-  return mockAsyncStorage;
-});
-
-jest.mock('react-native-code-push', () => {
-  const codePush = {
-    InstallMode: { ON_NEXT_RESTART: 'ON_APP_RESTART' },
-    CheckFrequency: { ON_APP_RESUME: 'ON_APP_RESUME' },
-  };
-  const cb = (_) => (app) => app;
-  Object.assign(cb, codePush);
-  return cb;
-});
-
-jest.mock('react-native-background-timer', () => ({
-  setTimeout: jest.fn(),
-  clearTimeout: jest.fn(),
-}));
-
-jest.mock('expo-web-browser', () => ({
-  openBrowserAsync: jest.fn(),
-}));
-
-jest.mock('expo-calendar', () => ({
-  createEventAsync: jest.fn(),
-  openEventInCalendar: jest.fn(),
-}));
-
-jest.mock('react-native-notifications', () => 'Notifications');
-jest.mock('react-native-snap-carousel', () => 'Caro');
-jest.mock('react-native-store-review', () => 'StoreReview');
--- template/jest.config.js
+++ template/jest.config.js
@@ -1,8 +1,9 @@
 module.exports = {
-  preset: 'react-native',
+  preset: '@testing-library/react-native',
+  testEnvironment: 'jsdom',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   setupFiles: ['./__mocks__/setup.js', './node_modules/react-native-gesture-handler/jestSetup.js'],
   transformIgnorePatterns: [
-    'node_modules/(?!(react-native|react-native-reanimated|react-native-redash|react-native-screens/native-stack|@react-navigation|@react-native-community|react-native-safari-view|react-native-code-push|react-native-iphone-x-helper|react-native-keyboard-aware-scroll-view|react-native-gesture-handler|@sentry/react-native)/)',
+    'node_modules/(?!(react-native|react-native-markdown-display|@ptomasroos/react-native-multi-slider|reactotron-react-native|react-native-flipper|react-native-reanimated|react-native-redash|react-native-screens/native-stack|@react-navigation|@react-native-community|react-native-safari-view|react-native-code-push|react-native-iphone-x-helper|react-native-keyboard-aware-scroll-view|react-native-gesture-handler|@sentry/react-native)/)',
   ],
 };
